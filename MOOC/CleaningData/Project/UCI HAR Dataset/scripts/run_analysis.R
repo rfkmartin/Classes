af3c1b1acdd06dc28a18523a79dca40ad4bd3a55
Robert F.K. Martin <rfkmartin@gmail.com>
2015-02-20 17:08:29 +0000

    * adding in features * paring down data

--- MOOC/CleaningData/Project/UCI HAR Dataset/scripts/run_analysis.R
+++ MOOC/CleaningData/Project/UCI HAR Dataset/scripts/run_analysis.R
@@ -1,25 +1,44 @@
+## read in features
+feats<-read.table("../features.txt")
+## select features with mean()
+idx<-grep("mean\\(\\)",feats$V2)
+
+
+## read in test data
 test.subject =read.table("../test/subject_test.txt")
 test.X       =read.table("../test/X_test.txt")
 test.y       =read.table("../test/y_test.txt")
-##TODO merge these together
+## merge these together
 test<-cbind(test.subject,test.y,test.X)
+## clear up some space
+remove(test.subject)
+remove(test.X)
+remove(test.y)
 
+## read in training data
 train.subject=read.table("../train/subject_train.txt")
 train.X      =read.table("../train/X_train.txt")
 train.y      =read.table("../train/y_train.txt")
-##TODO merge these together
+## merge these together
 train<-cbind(train.subject,train.y,train.X)
+## clear up some space
+remove(train.subject)
+remove(train.X)
+remove(train.y)
 
+## combine test and training data
 data<-rbind(test,train)
-##subject=rbind(test.subject,train.subject)
-##X      =rbind(test.X,train.X)
-##y      =rbind(test.y,train.y)
+## clear up some space
+remove(train)
+remove(test)
 
-## read in features
-feats<-read.table("../features.txt")
-## select features with mean() ?? meanFreq()
 ## pare down subject with mean() features
+names(data)<-c("Subject.ID","Activity.ID",as.character(feats$V2))
+mean.idx=c(1,2,idx+2)
+mean.data<-data[mean.idx]
 
 ## read activity
 acts<-read.table("../activity_labels.txt")
+names(acts)<-c("Activity.ID","Activity")
 ## label activities with text labels
+mean.data<-merge(acts,mean.data)
