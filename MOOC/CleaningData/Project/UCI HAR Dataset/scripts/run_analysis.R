3504bc31969621e37815bb9d947fce2bb348a2ec
Robert F.K. Martin <rfkmartin@gmail.com>
2015-02-20 03:22:31 +0000

    reading in data and combining

--- MOOC/CleaningData/Project/UCI HAR Dataset/scripts/run_analysis.R
+++ MOOC/CleaningData/Project/UCI HAR Dataset/scripts/run_analysis.R
@@ -1,6 +1,25 @@
-test.subject =read.csv("../test/subject_test.txt")
-test.X       =read.csv("../test/X_test.txt")
-test.y       =read.csv("../test/y_test.txt")
-train.subject=read.csv("../train/subject_train.txt")
-train.X      =read.csv("../train/X_train.txt")
-train.y      =read.csv("../train/y_train.txt")
+test.subject =read.table("../test/subject_test.txt")
+test.X       =read.table("../test/X_test.txt")
+test.y       =read.table("../test/y_test.txt")
+##TODO merge these together
+test<-cbind(test.subject,test.y,test.X)
+
+train.subject=read.table("../train/subject_train.txt")
+train.X      =read.table("../train/X_train.txt")
+train.y      =read.table("../train/y_train.txt")
+##TODO merge these together
+train<-cbind(train.subject,train.y,train.X)
+
+data<-rbind(test,train)
+##subject=rbind(test.subject,train.subject)
+##X      =rbind(test.X,train.X)
+##y      =rbind(test.y,train.y)
+
+## read in features
+feats<-read.table("../features.txt")
+## select features with mean() ?? meanFreq()
+## pare down subject with mean() features
+
+## read activity
+acts<-read.table("../activity_labels.txt")
+## label activities with text labels
